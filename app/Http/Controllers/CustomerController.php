f6044f482e91fdf33b71158b1f58565360f00b13
kushal kumar <kushaljindal92@gmail.com>
2023-03-05 09:32:45 -0600

    add customer data

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,8 +6,12 @@ use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * @TODO error logging
+ */
 class CustomerController extends Controller
 {
+
     public function index(){
         $customer =  Customer::all();
         return response()->json($customer);
@@ -59,18 +63,27 @@ class CustomerController extends Controller
 
     public function update(Request $request,$id){
 
-        $customer =  Customer::find($id);
-        $customer->name = $request->name;
-        $customer->email=$request->email;
-        $customer->phone=$request->phone;
-        $customer->address=$request->address;
-        $customer->city=$request->city;
-        $customer->state=$request->state;
-        $customer->customer_type=$request->customer_type;
-        $customer->birthdate=date($request->birthdate);
-        $customer->gender=$request->gender;
-        $customer->status=$request->status;
-        $customer->save();
-        return response()->json($customer);
+        $customer = Customer::findOrFail($id);
+        if(empty($customer)) {
+            return response()->json(["error" => 'Record not found!'], 400);
+        }
+        $validator = Validator::make($request->all(), array(
+            "name" => 'bail|required|max:40',
+            'email' => 'bail|required|email:rfc,dns',
+            "phone" => 'bail|required|numeric|digits:10',
+            "address"=>'bail|required|max:255',
+            "city"=>'bail|required|max:50',
+            "state"=>'bail|required|max:50',
+            "customer_type"=>'bail|numeric',
+            "birthdate"=>'bail|date',
+            "gender"=>"bail|bool",
+            "status"=>'bail|required|bool'
+        ));
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),400);
+        }
+        $customer->fill($request->all())->save();
+
+        return response()->json($customer,200);
     }
 }
